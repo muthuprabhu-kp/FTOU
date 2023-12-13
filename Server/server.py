4f316cecd78f5e6d75925d2eb5bdfcf011fb87d9
Muthu Prabhu <muthuprabhu.kp@gmail.com>
2023-12-14 02:20:56 +0530

    Added ssl support.

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,9 @@
 import getopt
 import random
 import socket
+import ssl
 import sys
+from contextlib import nullcontext, contextmanager
 
 import toml
 
@@ -35,37 +37,54 @@ def get_byte_object(data):
     }
 
 
+@contextmanager
+def ssl_context(condition, context_manager, sock):
+    if condition:
+        with context_manager.wrap_socket(sock, server_side=True) as ssl_sock:
+            yield ssl_sock
+    else:
+        yield sock
+
+
 def send_ack(tcp_conn, prt):
     tcp_conn.send((bytes(f'PRT:{prt};TYP:ACK;', 'utf-8') + b"\0" * 100)[:100])
 
 
 def start_server(config):
-    server_ip = config['SERVER']['SERVER_IP']
-    tcp_port = config['SERVER']['TCP_PORT']
-    udp_port_range = config['SERVER']['UDP_PORT_RANGE']
+    server_ip = config['server']['host']
+    tcp_port = config['server']['port']
+    udp_port_range = config['server']['udp_port_range']
+    is_ssl_enabled = config['ssl']['enabled']
+    cert_chain = config['ssl']['cert_chain']
+    private_key = config['ssl']['private_key']
     print(f'Listening on: {server_ip}:{tcp_port}')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_sock:
         tcp_sock.bind((server_ip, tcp_port))
         tcp_sock.listen()
-        conn, addr = tcp_sock.accept()
-        with conn:
-            print(f"Connected by {addr}")
-            udp_port = random.choice(udp_port_range)
-            udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            udp_sock.bind((server_ip, udp_port))
-            conn.send((bytes(f'TYP:UDP;PRT:{udp_port};HST:{server_ip};', 'utf-8') + b"\0" * 100)[:100])
-            while True:
-                data, uaddr = udp_sock.recvfrom(1024)
-                if uaddr[0] != addr[0] or not data:
-                    break
-                header = get_header(data)
-                send_ack(conn, header.get('PRT'))
-                bye_obj = get_byte_object(data)
-                seq = get_sequence_obj(addr[0] + ':' + header['FLE'])
-                seq.add(bye_obj)
-                # data = conn.recv(1024)
+        context = nullcontext()
+        if is_ssl_enabled:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            context.load_cert_chain(cert_chain, private_key)
+        with ssl_context(is_ssl_enabled, context, tcp_sock) as ssl_sock:
+            conn, addr = ssl_sock.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                udp_port = random.choice(udp_port_range)
+                udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                udp_sock.bind((server_ip, udp_port))
+                conn.send((bytes(f'TYP:UDP;PRT:{udp_port};HST:{server_ip};', 'utf-8') + b"\0" * 100)[:100])
+                while True:
+                    data, uaddr = udp_sock.recvfrom(1024)
+                    if uaddr[0] != addr[0] or not data:
+                        break
+                    header = get_header(data)
+                    send_ack(conn, header.get('PRT'))
+                    bye_obj = get_byte_object(data)
+                    seq = get_sequence_obj(addr[0] + ':' + header['FLE'])
+                    seq.add(bye_obj)
+                    # data = conn.recv(1024)
 
-                # conn.sendall(data)
+                    # conn.sendall(data)
 
 
 if __name__ == '__main__':
