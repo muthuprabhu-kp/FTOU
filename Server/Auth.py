d40c7d5e0f0d02b597214530d7a08872cfafaac2
Muthu Prabhu <muthuprabhu.kp@gmail.com>
2023-12-18 02:33:40 +0530

    Added Auth class.

---
+++ Server/Auth.py
@@ -0,0 +1,21 @@
+from pysondb import db
+import rsa
+
+
+class Auth:
+    def __init__(self, path):
+        self.db = db.getDb(path)
+        pass
+
+    def is_valid_user(self, signature, auth_data):
+        user_info = self.db.getBy({"user_id": auth_data['user_id']})
+        if not user_info:
+            return False
+        key = user_info['key']
+        try:
+            rsa.decrypt()
+            rsa.verify(auth_data, signature, key)
+        except Exception as e:
+            print(e)
+            return False
+        return True
